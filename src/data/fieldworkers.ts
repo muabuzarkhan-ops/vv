8877adf5552ef77c304f57ecd4abe457f0741c7e
muabuzarkhan-ops <muabuzarkhan@gmail.com>
2026-06-24 00:41:57 +0500

    add FieldWorkerProfile interface and initial field workers data

---
+++ src/data/fieldworkers.ts
@@ -0,0 +1,18 @@
+export interface FieldWorkerProfile {
+  id: string;
+  password: string;
+  name: string;
+}
+
+export const FIELD_WORKERS: FieldWorkerProfile[] = [
+  { id: 'collector01', password: 'collect01', name: 'Worker One' },
+  { id: 'collector02', password: 'collect02', name: 'Worker Two' },
+  { id: 'collector03', password: 'collect03', name: 'Worker Three' },
+  { id: 'collector04', password: 'collect04', name: 'Worker Four' },
+  { id: 'collector05', password: 'collect05', name: 'Worker Five' },
+  { id: 'collector06', password: 'collect06', name: 'Worker Six' },
+  { id: 'collector07', password: 'collect07', name: 'Worker Seven' },
+  { id: 'collector08', password: 'collect08', name: 'Worker Eight' },
+  { id: 'collector09', password: 'collect09', name: 'Worker Nine' },
+  { id: 'collector10', password: 'collect10', name: 'Worker Ten' },
+];
